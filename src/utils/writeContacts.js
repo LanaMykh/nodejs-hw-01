5256dec244fad06caa69d36ee24c5e71b1a83cc1
Svitlana Mykhailiuk <svetiklan@gmail.com>
2025-01-15 03:54:27 +0200

    step 1-3

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,7 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { writeFile } from "node:fs/promises";
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+    const contacts = await writeFile(PATH_DB,JSON.stringify(updatedContacts, null, 2));
+    return contacts;
+};
